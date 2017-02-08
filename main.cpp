6ab2d7a2b4b53f0b1c0f0c75cecc2f818c61b87e
Alexis Poncet <alexis.poncet@ens.fr>
2017-02-08 15:24:48 +0000

    Add GNU GPL license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/*
+Copyright (C) 2017 Alexis Poncet
+
+ChaoticBilliard is a computer program whose purpose is to compute the
+dynamics of chaotic billiards and show the trajectories both in physical
+space and phase space.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <QApplication>
 #include "gui/MainWindow.h"
 
